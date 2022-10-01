3d743e4ed6292cf790fc2de010d1cba8daca49ff
蔡依林 <2893176150@qq.com>
2022-10-02 04:36:05 +0800

    test01

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
-
+<script>
+import { defineComponent } from "vue";
+import { HelloWorld } from "./components/HelloWorld.vue";
+export default defineComponent({
+  name: "App"
+});
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
