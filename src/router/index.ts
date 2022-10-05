71625dcbee1c911aa58f0aad62e30c0b7c3dd490
蔡依林 <2893176150@qq.com>
2022-10-05 10:58:34 +0800

    feat(初始化): login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router"; // type：声明导入的是type类型
 
 const routes: RouteRecordRaw[] = [
   {
